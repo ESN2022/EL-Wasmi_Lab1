81e5bc67139ba4411c0bd3cd7ef28a21b6c147f2
AhmedWasmi <75042681+AhmedWasmi@users.noreply.github.com>
2023-01-15 16:07:22 +0100

    Validation du Led Chaser avec méthode de pooling
    - Modification de l'architecture Qsys en ajoutant les PIO de sortie pour les 8 LED, les PIO d'entrée pour le bouton poussoir et les commutateurs.
    - Mise à jour du fichier d'entête
    - Affectation des broches
    - Création de la partie logicielle pour réaliser le Led Chaser avec la méthode de pooling.
    
    -->Prochaine étape : amélioration du code en utilisant les interruptions

--- Lab1.vhd
+++ Lab1.vhd
@@ -7,8 +7,9 @@ entity Lab1 is
 		clk                         : in  std_logic;             --                        clk.clk
 		leds: out std_logic_vector(7 downto 0);                    --   leds_external_connection.export
 		push : in  std_logic                    := '0';             --   push_external_connection.export
-		switch: in  std_logic_vector(3 downto 0) := (others => '0')  -- switch_external_connection.export
-	);
+		switch: in  std_logic_vector(3 downto 0) := (others => '0');  -- switch_external_connection.export
+		reset                     : in  std_logic 
+		);
 	
 end entity Lab1;  
 	 
@@ -18,8 +19,9 @@ end entity Lab1;
             clk_clk                           : in  std_logic                    := 'X';             -- clk
             leds_external_connection_export   : out std_logic_vector(7 downto 0);                    -- export
             push_external_connection_export   : in  std_logic                    := 'X';             -- export
-            switch_external_connection_export : in  std_logic_vector(3 downto 0) := (others => 'X')  -- export
-        );
+            switch_external_connection_export : in  std_logic_vector(3 downto 0) := (others => 'X');  -- export
+				reset_reset_n                     : in  std_logic
+		  );
     end component Lab1_sys;
 	 
 begin
@@ -28,6 +30,7 @@ begin
             clk_clk                           => clk,                           --                        clk.clk
             leds_external_connection_export   => leds,   --   leds_external_connection.export
             push_external_connection_export   => push,   --   push_external_connection.export
-            switch_external_connection_export => switch  -- switch_external_connection.export
+            switch_external_connection_export => switch,  -- switch_external_connection.export
+				reset_reset_n							 => reset
         );
-		  end architecture rtl;
+end architecture rtl;
