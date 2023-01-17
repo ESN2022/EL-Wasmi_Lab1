0785a4aadf118d537c5505393473184380c829fd
AhmedWasmi <75042681+AhmedWasmi@users.noreply.github.com>
2023-01-17 19:50:06 +0100

    Correction du main interrupt

--- software/app/main_interrupt.c
+++ software/app/main_interrupt.c
@@ -3,10 +3,9 @@
 #include "altera_avalon_pio_regs.h"
 #include "sys/alt_irq.h"
 
-
 int speed = 50000;
 int led = 0;
-int chaser_on = 0;
+int chaser_state=0 ;
 
 static void irqhandler_push_button(void* context);
 static void irqhandler_switch(void* context);
@@ -19,12 +18,16 @@ int main()
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(SWITCH_BASE, 0x0);
     alt_ic_isr_register(PUSH_IRQ_INTERRUPT_CONTROLLER_ID, PUSH_IRQ, (void*)irqhandler_push_button, NULL, NULL);
     alt_ic_isr_register(SWITCH_IRQ_INTERRUPT_CONTROLLER_ID, SWITCH_IRQ, (void*)irqhandler_switch, NULL, NULL);
+
+
     while (1) {
-        if (chaser_on) {
+
+        if (chaser_state){
+
             IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE, (1 << led));
             led = (led + 1) % 8;
-            usleep(speed);
         }
+        usleep(speed);
     }
     return 0;
 }
@@ -32,7 +35,8 @@ int main()
 static void irqhandler_push_button(void* context)
 {
     alt_putstr("push button interruption\n");
-    chaser_on = !chaser_on;
+
+    	chaser_state = !chaser_state;
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BASE, 0x0);
 }
 
